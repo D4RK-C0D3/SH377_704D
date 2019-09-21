d8a5de5679849bd6ad3dd82476d7f3410b4cea85
Dipan Nama <dipannama468@gmail.com>
2019-09-21 16:56:28 +0530

    adding modules

---
+++ core/PY_L04D3R.sh
@@ -0,0 +1,105 @@
+#PY_L04D3R BY D4RK-C0D3 (DIPAN NAMA)
+#This code is only for termux users
+#You can use this script as a module
+#This module will help you to generate New loading effects for termux
+loader(){
+	clear
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo ''
+	echo '	  Termux is Loading...'
+	func1() {
+		sl=0.1
+		echo -ne '  \e[32m[                                  ](0%)\r'
+		sleep $sl
+		echo -ne '  [#                                 ] (3%)\r'
+		sleep $sl
+		echo -ne '  [##                                ] (6%)\r'
+		sleep $sl
+		echo -ne '  [###                               ] (9%)\r'
+		sleep $sl
+		echo -ne '  [####                              ] (12%)\r'
+		sleep $sl
+		echo -ne '  [#####                             ] (15%)\r'
+		sleep $sl
+		echo -ne '  [######                            ] (18%)\r'
+		sleep $sl
+		echo -ne '  [#######                           ] (21%)\r'
+		sleep $sl
+		echo -ne '  [########                          ] (24%)\r'
+		sleep $sl
+		echo -ne '  [#########                         ] (27%)\r'
+		sleep $sl
+		echo -ne '  [##########                        ] (30%)\r'
+		sleep $sl
+		echo -ne '  [###########                       ] (33%)\r'
+		sleep $sl
+		echo -ne '  [############                      ] (36%)\r'
+		sleep $sl
+		echo -ne '  [#############                     ] (39%)\r'
+		sleep $sl
+		echo -ne '  [##############                    ] (42%)\r'
+		sleep $sl
+		echo -ne '  [###############                   ] (45%)\r'
+		sleep $sl
+		echo -ne '  [################                  ] (48%)\r'
+		sleep $sl
+		echo -ne '  [#################                 ] (51%)\r'
+		sleep $sl
+		echo -ne '  [##################                ] (54%)\r'
+		sleep $sl
+		echo -ne '  [###################               ] (57%)\r'
+		sleep $sl
+		echo -ne '  [####################              ] (60%)\r'
+		sleep $sl
+		echo -ne '  [#####################             ] (63%)\r'
+		sleep $sl
+		echo -ne '  [######################            ] (66%)\r'
+		sleep $sl
+		echo -ne '  [#######################           ] (69%)\r'
+		sleep $sl
+		echo -ne '  [########################          ] (72%)\r'
+		sleep $sl
+		echo -ne '  [#########################         ] (75%)\r'
+		sleep $sl
+		echo -ne '  [##########################        ] (78%)\r'
+		sleep $sl
+		echo -ne '  [###########################       ] (81%)\r'
+		sleep $sl
+		echo -ne '  [############################      ] (84%)\r'
+		sleep $sl
+		echo -ne '  [#############################     ] (87%)\r'
+		sleep $sl
+		echo -ne '  [##############################    ] (90%)\r'
+		sleep $sl
+		echo -ne '  [###############################   ] (93%)\r'
+		sleep $sl
+		echo -ne '  [################################  ] (96%)\r'
+		sleep $sl
+		echo -ne '  [################################# ] (99%)\r'
+		sleep $sl
+		echo -ne '  [##################################](100%)\r'
+		sleep $sl
+		echo -ne '\n'
+		}
+	func1
+	clear
+}
+
+#Thanks for visiting and reviewing my code
+#All rights are resolbed by D4RK-COD3
